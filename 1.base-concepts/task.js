0b8f16c57134cd2ba34cbee9250e2c64bdb36560
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-11-09 18:53:44 +0400

    add task 1-4

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = Math.pow(b, 2) - (4 * a * c);
+  let x1 = (-b + Math.sqrt(d)) / (2 * a);
+  let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    let x = -b / (2 * a)
+    arr = [x];
+  } else {
+    
+    arr = [x1, x2];
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
